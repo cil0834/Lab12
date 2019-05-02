b8be218f5ce74c470e061372931293e32d9104fb
Caleb Lagge <calebl0834@gmail.com>
2019-05-02 10:00:31 -0500

    Added todo

---
+++ src/Todo.java
@@ -0,0 +1,36 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-05-02
+OU ID_______________: 113478036
+OU EMAIL____________: caleb.i.lagge-1@ou.edu
+
+Tasks:
+1. make ssn transient
+2. set visuals method
+3. form fill JPanel
+4. spanel
+5. JTextField to set values of FormObject
+6. import button
+7. export button
+8. add items to the frame
+
+Estimated Time (minutes):
+1. 5
+2. 10
+3. 10
+4. 5
+5. 15
+6. 20
+7. 20
+8. 5
+
+Actual Time (minutes):
+1. 5 
+2. 10
+3. 15
+4. 5
+5. 15
+6. 30
+7. 30
+8. 5
+*/}
